ee21fdf29dac5c63f2108dac86e60603b0711fcf
pr0phet <1199342@qq.com>
2018-01-09 12:41:10 +0800

    2

--- ALU.vhd
+++ ALU.vhd
@@ -1,7 +1,7 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL; --123
 
 entity ALU is
     Port ( clk : in  STD_LOGIC;
