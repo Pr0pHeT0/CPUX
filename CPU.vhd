d3ae16b193e670e8bd8e99ba029f35625016f089
pr0phet <1199342@qq.com>
2018-01-11 03:22:45 +0800

    alu

--- CPU.vhd
+++ CPU.vhd
@@ -12,4 +12,24 @@ end CPU;
 
 architecture Behavioral of CPU is
 
-component 
+component ALU
+    Port 
+    (   
+        clk : in  STD_LOGIC;
+        alu_srcA : in std_logic_vector(15 downto 0);
+        alu_srcB : in std_logic_vector(15 downto 0);
+        alu_out : out std_logic_vector(15 downto 0);
+        alu_op : in std_logic_vector(2 downto 0)
+    );
+end component;
+
+component ALU
+    Port 
+    (   
+        clk : in  STD_LOGIC;
+        alu_srcA : in std_logic_vector(15 downto 0);
+        alu_srcB : in std_logic_vector(15 downto 0);
+        alu_out : out std_logic_vector(15 downto 0);
+        alu_op : in std_logic_vector(2 downto 0)
+    );
+end component;
